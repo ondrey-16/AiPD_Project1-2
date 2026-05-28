6ddba0413462e964b3aa953c83ff130b9dbf1621
Ondrey <andrzejwrzesinski.2004@gmail.com>
2026-05-28 11:41:45 +0200

    Changed threshold

--- Source/VoiceVerifierComponent.cpp
+++ Source/VoiceVerifierComponent.cpp
@@ -206,7 +206,7 @@ void VoiceVerifierComponent::verifyVoice()
 
 	const float distance = getDistance(referenceVector, verifyVector);
 
-	const float threshold = 0.5f;
+	const float threshold = 0.47f;
 
 	const bool accepted = distance <= threshold;
 
